d1d5767c4c16a95cd17fba406a1f9200fea90d19
nguyentham95 <nguyentham95@gmail.com>
2022-06-18 22:59:41 +0700

    first commit

---
+++ checkso.js
@@ -0,0 +1,10 @@
+function kiemtra() {
+    let number = +document.getElementById("number").value;
+    if (number % 2 == 0) {
+        alert (number + " là số chẵn");
+    }
+    else {
+        alert (number + " là số lẻ");
+    }
+    
+}
